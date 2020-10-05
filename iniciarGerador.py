a1b3c6d79106e8add78033d0990236d7db73f2a0
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2020-10-05 19:02:38 -0300

    Update iniciarGerador.py

--- iniciarGerador.py
+++ iniciarGerador.py
@@ -1,11 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
-from dotenv import load_dotenv, find_dotenv
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import smtplib, ssl, yagmail
+from email import encoders
+import smtplib, ssl
 import pandas as pd
 import numpy as np
-import os, sys
+import os, sys 
 
 image     = Image.open('./assets/referencia.png')
 font_type = ImageFont.truetype('./assets/GreatVibes-Regular.ttf', 60)
@@ -15,16 +16,32 @@ tabelaCSV     = pd.read_csv('./pessoas.csv')
 tabelaPessoas = tabelaCSV['certificadoNome'].values
 tabelaEmail   = tabelaCSV['email'].values
 
-serverGmail    = smtplib.SMTP('smtp.gmail.com', 587)
+server         = smtplib.SMTP('smtp.gmail.com', 587)
+context        = ssl.create_default_context()
+destinatario   = ''
 gmail_user     = ''
 gmail_password = ''
 
 def geradorDeCertificado(nome):
-    draw.text(xy=(510,525), text=nome, fill='black', font=font_type)
+    draw.text(xy=(510,528), text=nome, fill='black', font=font_type)
     image.save('./certificados/{nome}.png'.format(nome=nome))
 
 def gerarCertificadoDoCSV():
     for i in tabelaPessoas:
         geradorDeCertificado(i)
 
-gerarCertificadoDoCSV()
+def gerarMensagem():
+    server.sendmail(gmail_user, 'mbkaue@gmail.com', 'email enviado pelo scrpt\nass,\nkaue')
+
+def enviarEmail():
+    try:
+        server.starttls(context=context)
+        server.login(gmail_user, gmail_password)
+        gerarMensagem()
+    except Exception as e:
+        print('erro:\n', e)
+    finally:
+        server.quit()
+
+def main():
+    print('message')
