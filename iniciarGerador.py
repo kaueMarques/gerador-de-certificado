29f6f933df2d3c528cbb5af4171f8d576c2b0d3d
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2020-10-05 15:19:06 -0300

    v1.0.0

---
+++ iniciarGerador.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw, ImageFont
+from dotenv import load_dotenv, find_dotenv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib, ssl, yagmail
+import pandas as pd
+import numpy as np
+import os, sys
+
+
+image     = Image.open('./assets/referencia.png')
+font_type = ImageFont.truetype('./assets/GreatVibes-Regular.ttf', 60)
+draw      = ImageDraw.Draw(image)
+
+tabelaCSV     = pd.read_csv('./pessoas.csv')
+tabelaPessoas = tabelaCSV['certificadoNome'].values
+tabelaEmail   = tabelaCSV['email'].values
+
+serverGmail    = smtplib.SMTP('smtp.gmail.com', 587)
+gmail_user     = ''
+gmail_passwprd = ''
+
+def geradorDeCertificado(nome):
+    draw.text(xy=(510,525), text=nome, fill='black', font=font_type)
+    image.save('./certificados/{nome}.png'.format(nome=nome))
+
+def gerarCertificadoDoCSV():
+    for i in tabelaPessoas:
+        geradorDeCertificado(i)
+
+gerarCertificadoDoCSV()
